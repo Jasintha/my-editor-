ff77835c59b7cc8cd83f0b0063ffa34e5ca41d1f
Chamika-virtuan <chamika@virtuan.io>
2022-05-05 13:23:37 +0530

    added service file

---
+++ src/app/core/projectservices/servicefile.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { createRequestOption } from '@shared/util/request-util';
+
+type EntityResponseType = HttpResponse<any>;
+type EntityArrayResponseType = HttpResponse<any[]>;
+
+@Injectable({ providedIn: 'root' })
+export class ServiceFileService {
+  public resourceUrl =  '/api/editor/servicefiles';
+
+  constructor(protected http: HttpClient) {}
+
+  create(serviceFile: any, uuid: string): Observable<EntityResponseType> {
+    return this.http.post<any>(`${this.resourceUrl}/${uuid}`, serviceFile, { observe: 'response' });
+  }
+
+  update(serviceFile: any, uuid: string): Observable<EntityResponseType> {
+    return this.http.put<any>(`${this.resourceUrl}/${uuid}`, serviceFile, { observe: 'response' });
+  }
+
+  find(id: string, uuid: string): Observable<EntityResponseType> {
+    return this.http.get<any>(`${this.resourceUrl}/${uuid}/${id}`, { observe: 'response' });
+  }
+
+  findByProjectId(id: string, uuid: string): Observable<EntityArrayResponseType> {
+    return this.http.get<any[]>(`${this.resourceUrl}/project/${uuid}/${id}`, { observe: 'response' });
+  }
+
+  query(req?: any, uuid?: string): Observable<EntityArrayResponseType> {
+    const options = createRequestOption(req);
+    return this.http.get<any[]>(`${this.resourceUrl}/${uuid}`, { params: options, observe: 'response' });
+  }
+
+  delete(id: string, uuid: string): Observable<HttpResponse<any>> {
+    return this.http.delete<any>(`${this.resourceUrl}/${uuid}/${id}`, { observe: 'response' });
+  }
+
+}
