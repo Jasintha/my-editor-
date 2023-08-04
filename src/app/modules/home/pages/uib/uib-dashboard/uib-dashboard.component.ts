68a86491fc5fb07918a172ccfa19c44ab1482ea8
hasinia <hasinimadhuwanthi1234@gmail.com>
2023-08-04 19:43:35 +0530

    added uib changes

---
+++ src/app/modules/home/pages/uib/uib-dashboard/uib-dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from "@angular/core";
+
+@Component({
+    selector: 'uib-dashboard-page',
+    templateUrl: './uib-dashboard.component.html',
+    styleUrls: ['./uib-dashboard.component.scss'],
+    encapsulation: ViewEncapsulation.None
+  })
+  export class UibDashboardPageComponent  {
+
+
+  }
+  
