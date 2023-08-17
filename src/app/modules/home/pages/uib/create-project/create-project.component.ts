ed8b4837facef4d2aa38ba6b644467d6516bcc79
hasinia <hasinimadhuwanthi1234@gmail.com>
2023-08-17 20:16:26 +0530

    add snackbar

--- src/app/modules/home/pages/uib/create-project/create-project.component.ts
+++ src/app/modules/home/pages/uib/create-project/create-project.component.ts
@@ -1,65 +1,82 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { RequirementService } from "@app/core/projectservices/requirement.service";
 import { UIBService } from "@app/core/projectservices/uib.service";
 import { da } from "date-fns/locale";
-
+import { SnackBarComponent } from "../snackbar/snackbar.component";
+import { computeMsgId } from "@angular/compiler";
 
 @Component({
-    selector: "create-uib-project",
-    templateUrl: "./create-project.component.html",
-    styleUrls: [
-      "./create-project.component.scss",
-    ],
-    encapsulation: ViewEncapsulation.None,
-  })
-export class CreateProjectComponent implements OnInit{
-    purposes = [
-        'Integration'
-    ]
+  selector: "create-uib-project",
+  templateUrl: "./create-project.component.html",
+  styleUrls: ["./create-project.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+})
+export class CreateProjectComponent implements OnInit {
+  purposes = ["Integration"];
 
-    projectForm = this.fb.group({
-        projectName: ['', Validators.required],
-        description: ['', Validators.required],
-        purpose: [null, Validators.required],
-    })
+  projectForm = this.fb.group({
+    projectName: ["", Validators.required],
+    description: ["", Validators.required],
+    purpose: [null, Validators.required],
+  });
 
-    projectUuid: string
+  projectUuid: string;
 
-    constructor(private fb: FormBuilder, private uibService: UIBService, private requirementService: RequirementService){}
+  constructor(
+    private fb: FormBuilder,
+    private uibService: UIBService,
+    private requirementService: RequirementService,
+    private snackBar: MatSnackBar
+  ) {}
   ngOnInit(): void {
-    this.requirementService.findAllDesignTreeData().subscribe((data)=>{
-      this.projectUuid = data.body[0].projectuuid
-    })
+    this.requirementService.findAllDesignTreeData().subscribe((data) => {
+      this.projectUuid = data.body[0].projectuuid;
+    });
   }
 
-        
   updateProfile() {
     this.projectForm.patchValue({
-        projectName: '',
-        description: '',
-        purpose: this.purposes[0]
+      projectName: "",
+      description: "",
+      purpose: this.purposes[0],
     });
   }
 
   onSubmit() {
     const newProject = {
-    requirements: [
+      requirements: [
         {
-            requirementUUID: "r0001",
-            description: "<p>ZxXX</p>"
+          requirementUUID: "r0001",
+          description: "<p>ZxXX</p>",
+        },
+      ],
+      name: this.projectForm.get(["projectName"]).value,
+      projectUuid: this.projectUuid,
+      epicCreateType: "new",
+      description: this.projectForm.get(["description"]).value,
+      status: "NEW",
+      epicuuid: "",
+      referenceName: this.projectForm.get(["projectName"]).value,
+    };
+    this.uibService
+      .createUIBProject(newProject, this.projectUuid)
+      .subscribe((value) => {
+        let msg = ''
+        if (value.status == 200) {
+           msg = 'Created Successfully!'
+        } else {
+          msg = 'Creation Failed!'
         }
-    ],
-    name: this.projectForm.get(['projectName']).value, 
-    projectUuid: this.projectUuid,
-    epicCreateType: "new",
-    description: this.projectForm.get(['description']).value,
-    status: "NEW",
-    epicuuid: "",
-    referenceName: this.projectForm.get(['projectName']).value, 
-    }
-    this.uibService.createUIBProject(newProject, this.projectUuid).subscribe((value)=> {
-      console.log(value)
-    })
+        this.snackBar.open(msg,'',{
+          duration: 2000
+        });
+      });
   }
 }
